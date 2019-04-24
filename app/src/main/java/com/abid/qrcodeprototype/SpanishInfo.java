927dbf10079618f9fbdb205058e3ec2d71fdbc1d
Mateen-Rahmat <mateen.rahmat5@gmail.com>
2019-04-24 23:50:30 +0430

    SpanishFIle

---
+++ app/src/main/java/com/abid/qrcodeprototype/SpanishInfo.java
@@ -0,0 +1,13 @@
+package com.abid.qrcodeprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpanishInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spanish_info);
+    }
+}
